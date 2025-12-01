e43f115aeee236616b4e26248b916cfaa995ebc7
Luis Tschurtschenthaler <business.luis.tschurtschenthaler@gmail.com>
2025-12-01 10:43:15 +0100

    feat: solved day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,44 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.runningFold(50) { position, instruction ->
+            position.rotate(instruction)
+        }.count { it == 0 }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.fold(50 to 0) { (position, totalZeros), instruction ->
+            val zerosCrossed = position.countZerosCrossed(instruction)
+            val newPosition = position.rotate(instruction)
+            newPosition to (totalZeros + zerosCrossed)
+        }.second
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun Int.rotate(instruction: String): Int {
+    val direction = instruction[0]
+    val steps = instruction.drop(1).toInt()
+    return when (direction) {
+        'L' -> (this - steps).mod(100)
+        'R' -> (this + steps).mod(100)
+        else -> this
+    }
+}
+
+private fun Int.countZerosCrossed(instruction: String): Int {
+    val direction = instruction[0]
+    val steps = instruction.drop(1).toInt()
+
+    return when (direction) {
+        'R' -> (this + steps) / 100
+        'L' -> when {
+            this == 0 -> steps / 100
+            steps >= this -> 1 + (steps - this) / 100
+            else -> 0
+        }
+        else -> 0
+    }
 }
